78d0ee1a15ad867272ee148e5319974caae68b09
Pelekh Ruslan <yura39@i.ua>
2019-04-12 17:51:03 +0300

    add new file

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    $('.slider-in-header').slick();
+    $('.slider-in-header').slick({
+    	prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        dots:true
+    });
 });
