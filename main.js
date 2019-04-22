cc5776186920955b4e3837db9506f9a40b252873
Pelekh Ruslan <yura39@i.ua>
2019-04-22 20:36:07 +0300

    fixed bag

--- main.js
+++ main.js
@@ -6,19 +6,12 @@ $(document).ready(function(){
     });
 
 
-     $('.popup-youtube').magnificPopup({
-     disableOn: 700,
-     type: 'iframe',
-     mainClass: 'mfp-fade',
-     removalDelay: 160,
-     preloader: false,
-   
-     fixedContentPos: false
+$('.popup-youtube').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
     });
-
-
-
 });
-
-
-   
